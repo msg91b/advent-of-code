e6cc99a215e73ba8e1aba780af29b51263153742
msg91b <mattgaitan@gmail.com>
2015-12-08 20:12:13 -0800

    Advent of Code Day 7

---
+++ 7/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+struct values {
+	string v1;
+	string v2;
+	string command;
+	string output;
+};
+
+void parse(map<string, uint16_t>& matches, vector<values>& myValues, int& index) {
+	string s, temp;
+	istringstream ss(s);
+	values operation;
+
+	getline(ss, temp, ' ');
+	if (temp == "NOT") {
+		operation.command = temp;
+		getline(ss, temp, ' ');
+		operation.v1 = temp;
+		getline(ss, temp, ' ');// -> throwaway
+		getline(ss, temp);
+		operation.output = temp;
+
+		myValues.push_back(operation);
+	}
+
+	else {
+		operation.v1 = temp;
+		getline(ss, temp, ' ');
+
+		if (temp == "->") {
+			getline(ss, temp);
+			operation.output = temp;
+			myValues.push_back(operation);
+
+			if (myValues.at(index).v1 != "lx")
+				matches[myValues.at(index).output] = stoi(myValues.at(index).v1);
+
+		}
+
+		else {
+			operation.command = temp;
+			getline(ss, temp, ' ');
+			operation.v2 = temp;
+			getline(ss, temp, ' '); // -> throwaway
+			getline(ss, temp);
+			operation.output = temp;
+			myValues.push_back(operation);
+		}
+	}
+	index++;
+}
+
+void shift(map<string, uint16_t>& matches, vector<values>& myValues) {
+	for (auto const& x : matches) {
+		for (auto &i : myValues) {
+			if (x.first == i.v1) {
+				if (i.command == "RSHIFT") {
+					matches[i.output] = x.second >> stoi(i.v2);
+				}
+				else if (i.command == "LSHIFT") {
+					matches[i.output] = x.second << stoi(i.v2);
+				}
+			}
+		}
+	}
+}
+
+void ao(map<string, uint16_t>& matches, vector<values>& myValues) {
+	for (auto const& x : matches) {
+		for (auto const& y : matches) {
+			for (auto &i : myValues) {
+				if (i.v1 == "1") {
+					uint16_t one = stoi(i.v1);
+					matches[i.output] = 1 & y.second;
+				}
+				else if ((i.v1 == x.first && i.v2 == y.first) || (i.v2 == x.first && i.v1 == y.first)) {
+					if (i.command == "AND")
+						matches[i.output] = x.second & y.second;
+					if (i.command == "OR")
+						matches[i.output] = x.second | y.second;
+				}
+			}
+		}
+	}
+}
+
+void not(map<string, uint16_t>& matches, vector<values>& myValues) {
+	for (auto const& x : matches) {
+		for (auto &i : myValues) {
+			if (x.first == i.v1) {
+				if (i.command == "NOT") {
+					matches[i.output] = ~x.second;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	string s;
+	int index(0);
+	ifstream input("input.txt");
+
+	map<string, uint16_t> matches;
+	vector<values> myValues;
+
+	while (getline(input, s)) {
+		parse(matches, myValues, index);
+	}
+
+	while (matches["a"] == 0) {
+		shift(matches, myValues);
+		ao(matches, myValues);
+		not(matches, myValues);
+	}
+
+
+
+	// print all matches
+	/*for (auto const& x : matches) {
+	output << x.first << ':' << x.second << endl;
+	}*/
+
+	cout << matches["a"] << endl;
+
+	system("pause");
+	return 0;
+}
